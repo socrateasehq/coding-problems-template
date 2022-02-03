ab48540bccf7b1e3b5eb7fafc74eb50d37184f57
Anshul <anshul@socratease.co>
2022-02-03 18:16:19 +0530

    java questions

--- demo/1_extract_1st_char/langs/php.php
+++
@@ -1,5 +0,0 @@
-function firstChar($str) {
-return substr($str, 0, 1);
-}
-$line = fgets(STDIN);
-echo firstChar($line);
---
+++ difficulty_1/2_minimum_number_swaps/langs/Java.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.*;
+
+class Solution
+{
+    public int minSwaps(int[] arr, int N)
+    {
+        int ans = 0;
+        int[] temp = Arrays.copyOfRange(arr, 0, N);
+        Arrays.sort(temp);
+        for (int i = 0; i < N; i++) 
+        {
+            if (arr[i] != temp[i]) 
+            {
+                ans++;
+                swap(arr, i, indexOf(arr, temp[i]));
+            }
+        }
+        return ans;
+    }
+    public void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public int indexOf(int[] arr, int ele)
+    {
+        for (int i = 0; i < arr.length; i++) 
+        {
+            if (arr[i] == ele) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+class MinSwaps
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] S = br.readLine().trim().split(" ");
+        int[] nums = new int[S.length];
+        for (int i = 0; i < S.length; i++) {
+            nums[i] = Integer.parseInt(S[i]);
+        }
+        Solution sol = new Solution();
+        int ans = sol.minSwaps(nums, S.length);
+        System.out.println(ans);
+    }
+}
