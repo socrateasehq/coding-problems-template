8a98bedabcf2c9d1f34e144f9ea8e645c741d33f
Anshul <anshul@socratease.co>
2022-02-11 20:27:19 +0530

    Add comments for boilerplates

--- difficulty_1/demo/1_extract_1st_char/langs/php.php
+++ difficulty_1/demo/1_extract_1st_char/langs/php.php
@@ -1,5 +1,8 @@
+//User Boilerplate
 function firstChar($str) {
-return substr($str, 0, 1);
+    return substr($str, 0, 1);
 }
+
+// Compiler Boilerplate
 $line = fgets(STDIN);
 echo firstChar($line);
